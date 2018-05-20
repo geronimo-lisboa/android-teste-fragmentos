41061abe49b1b580360d191a0aee40dcc29358f2
geronimo-lisboa <luciano.geronimo.fnord@gmail.com>
2018-05-20 13:08:31 -0300

    Fragmento adicionado à tela. Ainda falta conseguir interagir com ele.

--- app/src/main/java/estudosfragmento/geronimo/don/estudofragmentos/MainActivity.java
+++ app/src/main/java/estudosfragmento/geronimo/don/estudofragmentos/MainActivity.java
@@ -1,15 +1,17 @@
 package estudosfragmento.geronimo.don.estudofragmentos;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TesteFragmento.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +50,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        Log.d("TESTE_FRAGMENTO", uri.toString());
     }
 }
